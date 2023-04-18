fd455d991041fba4e109e91cbe596f18773d6407
rtf9981@outlook.com <rtf9981@outlook.com>
2023-04-19 00:16:10 +0530

    'addvalidation'

--- src/app/modules/admin/component/user/user.component.ts
+++ src/app/modules/admin/component/user/user.component.ts
@@ -1,21 +1,22 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { IUserModel } from 'src/app/interface/IUserModel';
 import { IResponse } from 'src/app/interface/iresponse';
-import { ApiResponseService } from 'src/app/service/api-response.service';
-
+import { ApiResponseService } from 'src/app/service/api-response.service' 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css'],
 })
 export class UserComponent {
+  isVisible=false;
+  submitted=false;
   public res: IResponse | any;
   selectedFile: File|any;
   constructor(private response: ApiResponseService) {
-    this.GetAllModule();
+    this.GetAllUser();
   }
-  GetAllModule() {
+  GetAllUser() {
     this.response.GetAllData('/User/getAll').subscribe((response) => {
       this.res = response;
       // console.log(this.res)
@@ -36,21 +37,26 @@ export class UserComponent {
     }
   }
   userForm = new FormGroup({
-    fullName: new FormControl(),
-    mobile: new FormControl(),
-    email: new FormControl(),
-    type: new FormControl(),
-    userPassword: new FormControl(),
+    fullName: new FormControl('',[Validators.required, Validators.maxLength(20)]), 
+    mobile: new FormControl('',[Validators.required, Validators.maxLength(10)]),
+    email: new FormControl('',[Validators.required, Validators.email]),
+    type: new FormControl(''),
+    userPassword: new FormControl('',[Validators.required, Validators.minLength(6)]),
   });
   Submit(){
+    this.submitted=true;
+    if(this.userForm.invalid)
+    {
+      return;
+    }
      var formData=new FormData();
     let model={
       FullName: this.userForm.value.fullName ?? "",
       Mobile: this.userForm.value.mobile ?? "",
       Email: this.userForm.value.email ?? "",
       ImageFile: this.selectedFile,
-      Type: this.userForm.value.type,
-      UserPassword: this.userForm.value.userPassword,
+      Type: this.userForm.value.type??"",
+      UserPassword: this.userForm.value.userPassword??"",
     }
     formData.append("FullName",model.FullName);
     formData.append("Mobile",model.Mobile);
@@ -59,10 +65,15 @@ export class UserComponent {
     formData.append("Type",model.Type);
     formData.append("UserPassword",model.UserPassword);
     this.response.PostData("/User/add",formData).subscribe(
-    (response)=>{
-      console.log('response',response);
-      this.GetAllModule();
-
-     });
+      (response) => { this.res = response;
+        if(this.res.status==true)
+        {
+          alert(this.res.message);
+        }
+        else{
+          alert(this.res.message)
+        }
+        this.GetAllUser();
+       });
 }
 }
