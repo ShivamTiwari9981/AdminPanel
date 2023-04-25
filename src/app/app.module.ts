d289ec889938d276579d782aa2c0b90b7afeed7d
Shivam Tiwari <shivamtiwari8756@gmail.com>
2023-04-25 14:22:11 +0530

    removeCode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { ForgetPasswordComponent } from './components/forget-password/forget-password.component';
@@ -14,8 +13,6 @@ import{ReactiveFormsModule,FormsModule}from '@angular/forms';
 import { ApiResponseService } from '../app/service/api-response.service';
 import { LogoutComponent } from './components/logout/logout.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgbToastModule } from '@ng-bootstrap/ng-bootstrap';
-import { NgIf } from '@angular/common';
 import { CookieService } from 'ngx-cookie-service';
 
 
@@ -32,13 +29,10 @@ import { CookieService } from 'ngx-cookie-service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule,
     AdminModule,
     ReactiveFormsModule,
     FormsModule,
     BrowserAnimationsModule,
-    NgbToastModule,
-    NgIf
   ],
 
   providers: [ApiResponseService,CookieService],
